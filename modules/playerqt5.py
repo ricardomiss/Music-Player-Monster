5fbf7506f5a8f95a45682ae3442907bdc89e9a83
Angel Ricardo Miss Blanco <angelitomiss@yahoo.com.mx>
2024-06-24 22:00:40 -0600

    se agregaron los iconos

--- modules/playerqt5.py
+++ modules/playerqt5.py
@@ -20,7 +20,7 @@ class Player(QMainWindow):
     def __init__(self):
         super().__init__()
         #Configuracion de la ventana
-        self.setGeometry(0, 0, 600, 280)
+        self.setGeometry(config["windowX"], config["windowY"], 600, 280)
         self.setWindowTitle("Project Player")        
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
@@ -46,10 +46,55 @@ class Player(QMainWindow):
         self.minimize_button = QPixmap(iconosDirectory + "Minimize.png")
         self.minimize_label = QLabel(self)
         self.minimize_label.setPixmap(self.minimize_button)
-        self.minimize_label.setGeometry(520, -4, 40, 40)
+        self.minimize_label.setGeometry(505, -4, 40, 40)
         self.minimize_label.mousePressEvent = self.minimize_window
-    
+
+        #Contenido
+        self.NightMode_button = QPixmap(iconosDirectory + config[mode]["buttonMode"])
+        self.NightMode_label = QLabel(self)
+        self.NightMode_label.setPixmap(self.NightMode_button)
+        self.NightMode_label.setGeometry(550, 40, 40, 40)
+        self.NightMode_label.mousePressEvent = self.NightMode_Toggle
+
+        #TODO: Barra de volumen
+
+        self.Backward_button = QPixmap(iconosDirectory + config[mode]["buttonBackward"])
+        self.Backward_label = QLabel(self)
+        self.Backward_label.setPixmap(self.Backward_button)
+        self.Backward_label.setGeometry(230, 200, 40, 40)
+
+        self.Player_button = QPixmap(iconosDirectory + config[mode]["buttonPlay"])
+        self.Player_label = QLabel(self)
+        self.Player_label.setPixmap(self.Player_button)
+        self.Player_label.setGeometry(300, 200, 40, 40)
+
+        self.Forward_button = QPixmap(iconosDirectory + config[mode]["buttonForward"])
+        self.Forward_label = QLabel(self)
+        self.Forward_label.setPixmap(self.Forward_button)
+        self.Forward_label.setGeometry(350, 200, 40, 40)
+
+        self.Title = QLabel(self)
+        self.Title.setText("Titulo")
+        self.Title.setGeometry(200, 70, 200, 20)
+        self.Title.setStyleSheet("color: white; font-size: 30px; font-family: Arial;")
+        self.Title.setAlignment(Qt.AlignCenter)
+
+        self.Artist = QLabel(self)
+        self.Artist.setText("Artist")
+        self.Artist.setGeometry(200, 100, 200, 20)
+        self.Artist.setStyleSheet("color: white; font-size: 15px; font-family: Arial;")
+        self.Artist.setAlignment(Qt.AlignCenter)
+
+        self.Source = QLabel(self)
+        self.Source.setText("Source")
+        self.Source.setGeometry(10, 210, 200, 20)
+        self.Source.setStyleSheet("color: white; font-size: 15px; font-family: Arial;")
+        self.Source.setAlignment(Qt.AlignCenter)
+
     def close_window(self, event):
+        config["windowX"] = self.x()
+        config["windowY"] = self.y()
+        modifConfig(configDirectory, config)
         self.close()
     
     def minimize_window(self, event):
@@ -62,3 +107,18 @@ class Player(QMainWindow):
         delta = event.globalPos() - self.oldPos
         self.move(self.x() + delta.x(), self.y() + delta.y())
         self.oldPos = event.globalPos()
+    
+    def NightMode_Toggle(self, event):
+        config["NightModeAuto"] = not config["NightModeAuto"]
+        modifConfig(configDirectory, config)
+        self.update_elements()
+
+    def update_elements(self):
+        if config["NightModeAuto"]:
+            mode = "NightMode"
+        else:
+            mode = "LightMode"
+        self.frame_photo = QPixmap(framesDirectory + config[mode]["frame"])
+        self.frame_label.setPixmap(self.frame_photo)
+        self.NightMode_button = QPixmap(iconosDirectory + config[mode]["buttonMode"])
+        self.NightMode_label.setPixmap(self.NightMode_button)
